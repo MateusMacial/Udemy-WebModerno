6adc420bb456401cb932c4023a8e3a122fb00e01
Mateus Macial <mateus_macial@hotmail.com>
2021-07-08 13:19:22 -0300

    Modulo 1 aula 08/07/2021

---
+++ sesao_01/Fundamentos/02_Comentario.js
@@ -0,0 +1,18 @@
+//comentario de unica linha
+console.log("linha 1");
+
+/*
+comentario
+de multiplas
+linhas
+*/
+console.log("linha 2");
+
+/*
+*comentario de
+*multiplas
+*linhas
+*alternativo
+*ok
+*/
+console.log("linha 3");
